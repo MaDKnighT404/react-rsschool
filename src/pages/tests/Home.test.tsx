e9519de18c3368f9d7c26091c53568bc97506c45
George Koloidi <Battleaxe@bk.ru>
2023-03-15 21:52:12 +0100

    fix: test with NotFound page

--- src/pages/tests/Home.test.tsx
+++ src/pages/tests/Home.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { MemoryRouter, Router } from 'react-router';
 import Home from '../Home';
 
 it('display Home page', async () => {
--- src/pages/tests/NotFound.test.tsx
+++ src/pages/tests/NotFound.test.tsx
@@ -1,6 +1,11 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import NotFound from '../NotFound';
 
-it('display Home page', async () => {
-  render(<NotFound />);
+it('display NotFound page', async () => {
+  render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>
+  );
 });
