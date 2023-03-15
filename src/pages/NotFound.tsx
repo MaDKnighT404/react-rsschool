7cb0a43966fc2cc3c26c08f8224925b611763132
George Koloidi <Battleaxe@bk.ru>
2023-03-15 21:19:26 +0100

    feat: add header on NotFound page???

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,20 @@
-import styles from './styles/NotFound.module.scss';
+import { NavLink } from 'react-router-dom';
 import { Footer } from '../components';
+import styles from './styles/NotFound.module.scss';
+
 const NotFound = () => {
   return (
     <>
+      <div className={styles.header}>
+        <nav className={styles.headerNavigation}>
+          <p className={styles.headerLinks}>
+            <NavLink to="/" className={styles.headerLink} aria-current="true">
+              Press Here
+            </NavLink>
+            to return on main page
+          </p>
+        </nav>
+      </div>
       <div className={styles.wrapper}>
         <img className={styles.errorImg} src="../assets/404error.png" alt="error picture" />
       </div>
