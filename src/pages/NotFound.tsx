45e0ac374fee673fa0c3e51cb54b7134b6a7414d
George Koloidi <Battleaxe@bk.ru>
2023-03-15 21:05:00 +0100

    feat: remove NotFound page from single route. Add style for scrollbar

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,13 @@
 import styles from './styles/NotFound.module.scss';
+import { Footer } from '../components';
 const NotFound = () => {
   return (
-    <div className={styles.wrapper}>
-      <img className={styles.errorImg} src="../assets/404error.png" alt="error picture" />
-    </div>
+    <>
+      <div className={styles.wrapper}>
+        <img className={styles.errorImg} src="../assets/404error.png" alt="error picture" />
+      </div>
+      <Footer />
+    </>
   );
 };
 
