42d9b7c356cd45beb681c871b804a289e15aef21
George Koloidi <Battleaxe@bk.ru>
2023-03-16 14:13:32 +0100

    refactor: change Header RC to class. Add div with page title

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,22 +4,9 @@ import styles from './styles/NotFound.module.scss';
 
 const NotFound = () => {
   return (
-    <>
-      <div className={styles.header}>
-        <nav className={styles.headerNavigation}>
-          <p className={styles.headerLinks}>
-            <NavLink to="/" className={styles.headerLink} aria-current="true">
-              Press Here
-            </NavLink>
-            to return on main page
-          </p>
-        </nav>
-      </div>
-      <div className={styles.wrapper}>
-        <img className={styles.errorImg} src="../assets/404error.png" alt="error picture" />
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <img className={styles.errorImg} src="../assets/404error.png" alt="error picture" />
+    </div>
   );
 };
 
--- src/pages/tests/NotFound.test.tsx
+++ src/pages/tests/NotFound.test.tsx
@@ -1,11 +1,6 @@
 import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router';
 import NotFound from '../NotFound';
 
 it('display NotFound page', async () => {
-  render(
-    <MemoryRouter>
-      <NotFound />
-    </MemoryRouter>
-  );
+  render(<NotFound />);
 });
