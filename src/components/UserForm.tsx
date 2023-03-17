1adddbe11f12d843bae34e66e3dfe8fd1c3a37c1
George Koloidi <Battleaxe@bk.ru>
2023-03-18 00:12:22 +0100

    feat: add simple validation to 3 input

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,27 +1,148 @@
-import { Component } from 'react';
+import { ChangeEvent, Component } from 'react';
 import styles from './styles/UserForm.module.scss';
 
-class UserForm extends Component {
+interface FormValues {
+  name: string;
+  telephone: string;
+  email: string;
+  birthday: string;
+  gender: string;
+  photo: File | null;
+  skills: {
+    html: boolean;
+    css: boolean;
+    javascript: boolean;
+    typescript: boolean;
+    jest: boolean;
+    react: boolean;
+  };
+  notifications: boolean;
+}
+
+interface UserFormState {
+  values: FormValues;
+  errors: Partial<Record<keyof FormValues, string>>;
+}
+
+class UserForm extends Component<unknown, UserFormState> {
+  constructor(props: unknown) {
+    super(props);
+
+    this.state = {
+      values: {
+        name: '',
+        telephone: '',
+        email: '',
+        birthday: '',
+        gender: '',
+        photo: null,
+        skills: {
+          html: false,
+          css: false,
+          javascript: false,
+          typescript: false,
+          jest: false,
+          react: false,
+        },
+        notifications: false,
+      },
+      errors: {},
+    };
+  }
+
+  handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    const errors = { ...this.state.errors };
+
+    this.setState((prevState) => ({
+      values: {
+        ...prevState.values,
+        [name]: value,
+      },
+      errors,
+    }));
+  };
+
+  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const { errors, values } = this.state;
+
+    if (values.name.length < 3) {
+      errors.name = 'Name must be 3 letter minimum';
+    } else {
+      delete errors.name;
+    }
+
+    if (values.telephone.length < 3) {
+      errors.telephone = 'telephone must be 3 letter minimum';
+    } else {
+      delete errors.telephone;
+    }
+
+    if (values.email.length < 3) {
+      errors.email = 'email must be 3 letter minimum';
+    } else {
+      delete errors.email;
+    }
+
+    this.setState(() => ({
+      errors,
+    }));
+
+    if (Object.keys(errors).length > 0) {
+      console.log('Validation errors:', errors);
+      return;
+    }
+
+    console.log('Form values:', values);
+  };
+
   render() {
+    const { values, errors } = this.state;
     return (
-      <form className={styles.form} action="" lang="en">
+      <form className={styles.form} action="" onSubmit={this.handleSubmit}>
         <h2 className={styles.formTitle}>Form</h2>
         <fieldset className={styles.formFieldset}>
           <legend>Contact information</legend>
           <label htmlFor="name" className={styles.formLabel}>
             Name
           </label>
-          <input className={styles.formInputText} id="name" type="text" />
+          <input
+            className={styles.formInputText}
+            id="name"
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={this.handleInputChange}
+          />
+          {errors.name && <div className={styles.formError}>{errors.name}</div>}
           <br />
-          <label htmlFor="name" className={styles.formLabel}>
+          <label htmlFor="telephone" className={styles.formLabel}>
             Telephone
           </label>
-          <input className={styles.formInputText} id="name" type="text" />
+          <input
+            className={styles.formInputText}
+            id="telephone"
+            type="text"
+            name="telephone"
+            value={values.telephone}
+            onChange={this.handleInputChange}
+          />
+          {errors.telephone && <div className={styles.formError}>{errors.telephone}</div>}
           <br />
           <label htmlFor="name" className={styles.formLabel}>
             Email
           </label>
-          <input className={styles.formInputText} id="name" type="text" />
+          <input
+            className={styles.formInputText}
+            id="name"
+            type="text"
+            name="email"
+            value={values.email}
+            onChange={this.handleInputChange}
+          />
+          {errors.email && <div className={styles.formError}>{errors.email}</div>}
           <br />
         </fieldset>
 
@@ -82,7 +203,9 @@ class UserForm extends Component {
           <div className={styles.formToggleSwitch}></div>
         </label>
         <br />
-        <button className={styles.formButtonSubmit} type="submit">Submit</button>
+        <button className={styles.formButtonSubmit} type="submit">
+          Submit
+        </button>
       </form>
     );
   }
