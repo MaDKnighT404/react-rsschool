e098cf8950de9a55f1cf3cf95351e87d4a69408c
George Koloidi <Battleaxe@bk.ru>
2023-03-17 18:29:54 +0100

    feat: complete form html and css structure

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -7,7 +7,7 @@ class UserForm extends Component {
       <form className={styles.form} action="" lang="en">
         <h2 className={styles.formTitle}>Form</h2>
         <fieldset className={styles.formFieldset}>
-          <legend> Contact information</legend>
+          <legend>Contact information</legend>
           <label htmlFor="name" className={styles.formLabel}>
             Name
           </label>
@@ -26,11 +26,11 @@ class UserForm extends Component {
         </fieldset>
 
         <fieldset className={styles.formFieldset}>
-          <legend> Personal information</legend>
+          <legend>Personal information</legend>
           <label htmlFor="birthday" className={styles.formLabel}>
             Birthday
           </label>
-          <input className={styles.formInputText} id="birthday" type="date" lang="en" />
+          <input className={styles.formInputText} id="birthday" type="date" />
           <br />
           <label htmlFor="gender" className={styles.formLabel}>
             Gender
@@ -40,13 +40,49 @@ class UserForm extends Component {
             <option value="male">Male</option>
           </select>
           <br />
-          <label className={styles.formToggle}>
-            <span className={styles.formToggleLabel}>Have a car</span>
-            <input className={styles.formToggleCheckbox} type="checkbox" />
-            <div className={styles.formToggleSwitch}></div>
+          <label htmlFor="birthday" className={styles.formLabel}>
+            Photo
           </label>
+          <input
+            className={styles.formInputText}
+            id="birthday"
+            type="file"
+            accept="image/png, image/jpeg"
+          />
           <br />
         </fieldset>
+
+        <fieldset className={(styles.formFieldset, styles.formFieldsetCheckbox)}>
+          <legend> Skills</legend>
+          <label className={styles.formCheckbox} htmlFor="html">
+            <input id="html" type="checkbox" /> Html
+          </label>
+          <label className={styles.formCheckbox} htmlFor="css">
+            <input id="css" type="checkbox" /> Css
+          </label>
+          <label className={styles.formCheckbox} htmlFor="javascript">
+            <input id="javascript" type="checkbox" /> JavaScript
+          </label>
+          <label className={styles.formCheckbox} htmlFor="typescript">
+            <input id="typescript" type="checkbox" /> TypeScript
+          </label>
+          <label className={styles.formCheckbox} htmlFor="jest">
+            <input id="jest" type="checkbox" /> Jest
+          </label>
+          <label className={styles.formCheckbox} htmlFor="react">
+            <input id="react" type="checkbox" /> React
+          </label>
+        </fieldset>
+
+        <label className={styles.formToggle}>
+          <span className={styles.formToggleLabel}>
+            I want to receive notifications about promo, sales, etc.
+          </span>
+          <input className={styles.formToggleCheckbox} type="checkbox" />
+          <div className={styles.formToggleSwitch}></div>
+        </label>
+        <br />
+        <button className={styles.formButtonSubmit} type="submit">Submit</button>
       </form>
     );
   }
