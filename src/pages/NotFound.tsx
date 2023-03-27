4e3e3bcf8292cc506a83e5f45b799947c6c58d54
George Koloidi <Battleaxe@bk.ru>
2023-03-27 13:24:26 +0200

    refactor: rewrite Home, About, NotFound to functional comonent

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,14 +1,11 @@
-import { Component } from 'react';
 import styles from './styles/NotFound.module.scss';
 
-class NotFound extends Component {
-  render() {
-    return (
-      <div className={styles.wrapper}>
-        <img className={styles.errorImg} src="../assets/404error.png" alt="error picture" />
-      </div>
-    );
-  }
-}
+const NotFound = () => {
+  return (
+    <div className={styles.wrapper}>
+      <img className={styles.errorImg} src="../assets/404error.png" alt="error picture" />
+    </div>
+  );
+};
 
 export default NotFound;
