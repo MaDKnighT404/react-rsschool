4c49c35ffa5f033f8a538d50919304b55b67600d
George Koloidi <Battleaxe@bk.ru>
2023-03-27 14:06:14 +0200

    refactor: rewrite Input to functional comonent and use hooks

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,40 +1,24 @@
-import { ChangeEvent, Component } from 'react';
 import { AiOutlineSearch } from 'react-icons/Ai';
-import { InputState } from 'types/types';
+import { useState, useEffect, ChangeEvent } from 'react';
 import styles from './styles/Input.module.scss';
 
-class Input extends Component<Record<string, unknown>, InputState> {
-  state: InputState = {
-    value: localStorage.getItem('inputValue') || '',
-  };
+function Input() {
+  const [value, setValue] = useState<string>(() => localStorage.getItem('inputValue') || '');
 
-  handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    this.setState({ value });
-  };
-
-  componentDidMount() {
-    const inputValue = localStorage.getItem('inputValue') ?? '';
-    this.setState({ value: inputValue });
-  }
-
-  componentWillUnmount() {
-    const { value } = this.state;
+  useEffect(() => {
     localStorage.setItem('inputValue', value);
-  }
+  }, [value]);
 
-  render() {
-    return (
-      <div className={styles.inputContainer}>
-        <AiOutlineSearch className={styles.inputIcon} />
-        <input
-          value={this.state.value}
-          onChange={this.handleChange}
-          placeholder="Search"
-          className={styles.input}
-        />
-      </div>
-    );
-  }
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <div className={styles.inputContainer}>
+      <AiOutlineSearch className={styles.inputIcon} />
+      <input value={value} onChange={handleChange} placeholder="Search" className={styles.input} />
+    </div>
+  );
 }
+
 export default Input;
