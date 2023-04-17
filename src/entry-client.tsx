ae57405c1f59b940090493ab836e0fe6bb7948f2
George Koloidi <Battleaxe@bk.ru>
2023-04-17 20:27:11 +0200

    feat: renderToPipeableStream complete!!!

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from './redux/store';
 import App from './App';
+import store from './redux/store';
 import './index.scss';
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.hydrateRoot(
+  document.getElementById('root') as HTMLElement,
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
