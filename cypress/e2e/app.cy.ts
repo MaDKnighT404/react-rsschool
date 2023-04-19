85224caa76df45b2a4b476751fa98d63f6b0c606
George Koloidi <Battleaxe@bk.ru>
2023-04-19 13:30:24 +0200

    feat: start adding tests

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,9 +1,34 @@
-describe('Assert Home page', () => {
-  it('Header, main and footer are rendered', () => {
+describe('app', () => {
+  it('Header, main and footer are rendered on Home Page', () => {
     cy.visit('http://localhost:3000/');
     cy.get('header').should('be.visible');
     cy.get('main').should('be.visible');
     cy.get('footer').should('be.visible');
+  });
+});
+
+describe('Links in header are working', () => {
+  it('Home link', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('[data-testid="home-link"]').contains('Home').click();
+
+    cy.url().should('include', '/');
+  });
+
+  it('About link', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('[data-testid="about-link"]').contains('About').click();
+
+    cy.url().should('include', '/about');
+
+    cy.get('p').contains('Lorem');
+  });
+
+  it('Form link', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('[data-testid="form-link"]').contains('Form').click();
+
+    cy.url().should('include', '/form');
   });
 
   it('Test for remove loading on coverage', () => {
